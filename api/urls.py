87bf3e7e3d76609fffa6fd042cdd2532b0dd5491
curi_u <cucmberium@clarkdale.sakura.ne.jp>
2018-02-04 22:03:56 +0900

    Fix typo

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from api import views
 
 urlpatterns = [
     url(r'users/recommendations.json$', views.recommendations, name='recommendations'),
-    url(r'users/similar_users.json$', views.similar_user, name='similar_users'),
+    url(r'users/similar_users.json$', views.similar_users, name='similar_users'),
 ]
