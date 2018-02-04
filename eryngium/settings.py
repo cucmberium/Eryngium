1f49daa7d0bdceef5d68c453808d04cac09ae781
curi_u <cucmberium@clarkdale.sakura.ne.jp>
2018-02-04 19:37:51 +0900

    一部設定をsettings_localに移動

--- eryngium/settings.py
+++ eryngium/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = eryngium.settings_local.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = eryngium.settings_local.ALLOWED_HOST
 
 
 # Application definition
